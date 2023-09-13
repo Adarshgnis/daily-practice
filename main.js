479603921ca329e6e7da90a3febc9c8295168d46
- <=>
2023-09-13 09:31:36 +0530

    daily practice

---
+++ main.js
@@ -0,0 +1,29 @@
+const arr = [1,2,3,4,5]
+// console.log(arr[4])
+// const arr2 = arr
+// arr2.push = 6
+// console.log(arr)
+// console.log(arr2)
+// arr2.pop()
+// console.log(arr2)
+// console.log(arr2 === arr) 
+// arr.pop()
+// arr.unshift(0)
+// arr.shift()
+// const arr2 = [6,7,8,9,10,[11,12]]
+// const newArr =  arr.concat(arr2)
+// console.log(newArr)
+
+// const newArr = arr.splice(0,2)
+// console.log(newArr)
+console.log(arr)
+
+// const checkNum = 5
+
+// if(arr.includes(checkNum)) {
+//     console.log(`${checkNum} is present in the array`)
+// } else {
+//     console.log(`${checkNum} is not present in the array`)
+// }
+
+// console.log(arr.indexOf(5))
